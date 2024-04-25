af9d45a0470f3d8a8b433cf92c2c61dd6c7e064b
sreekanthcsree <sreekanthsss988@gmail.com>
2024-04-25 10:57:09 +0530

    First Commit

---
+++ SpringJenkinsApplication/SpringJenkinsApplication/src/main/java/com/dex/SpringJenkinsApplication/SpringJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.dex.SpringJenkinsApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
