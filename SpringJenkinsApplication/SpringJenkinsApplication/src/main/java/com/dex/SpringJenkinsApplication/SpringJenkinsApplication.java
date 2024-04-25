808a344f20a29718d2c08650ee355b21c30ffe51
sreekanthcsree <sreekanthsss988@gmail.com>
2024-04-25 12:00:10 +0530

    First Commit

--- SpringJenkinsApplication/SpringJenkinsApplication/src/main/java/com/dex/SpringJenkinsApplication/SpringJenkinsApplication.java
+++ SpringJenkinsApplication/SpringJenkinsApplication/src/main/java/com/dex/SpringJenkinsApplication/SpringJenkinsApplication.java
@@ -1,12 +1,25 @@
 package com.dex.SpringJenkinsApplication;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SpringJenkinsApplication {
-
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringApplication.class);
+	
+	@PostConstruct
+	public void hello() {
+		logger.info("code executed successfully...!");
+	}
 	public static void main(String[] args) {
+		logger.info("code executed successfully second commit...!");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
